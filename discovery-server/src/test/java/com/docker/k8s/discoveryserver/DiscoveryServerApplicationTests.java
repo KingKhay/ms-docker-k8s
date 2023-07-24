14f241426ec017024238d90d64bad77abb7d29b9
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-24 10:47:46 +0000

    initial-commit -> discovery server

---
+++ discovery-server/src/test/java/com/docker/k8s/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.k8s.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
