1c271c6239cba1e625d0f038ee8a39ad1eb11377
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-25 14:23:20 +0000

    refactor department controller

--- department-service/src/main/java/com/docker/k8s/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/docker/k8s/departmentservice/controller/DepartmentController.java
@@ -3,6 +3,8 @@ package com.docker.k8s.departmentservice.controller;
 import com.docker.k8s.departmentservice.model.Department;
 import com.docker.k8s.departmentservice.service.DepartmentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -14,7 +16,7 @@ public class DepartmentController {
 
     private final DepartmentService departmentService;
 
-    @GetMapping("/{id}")
+    @GetMapping("/employee/{id}")
     Department getDepartmentOfEmployee(@PathVariable UUID id){
         return departmentService.findDepartmentOfEmployee(id);
     }
@@ -22,6 +24,16 @@ public class DepartmentController {
     @PostMapping
     Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    Department findDepartmentById(@PathVariable UUID id){
+        return departmentService.findDepartmentById(id);
+    }
+
+    @GetMapping()
+    Page<Department> findAllDepartments(Pageable pageable){
+        return departmentService.findAllDepartments(pageable);
     }
 
     @GetMapping("/hello")
