1fb0f1317f55e356737d44c9539b33f1b5ecaa06
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-25 08:58:03 +0000

    added controller for employee service

---
+++ employee-service/src/main/java/com/docker/k8s/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.docker.k8s.employeeservice.controller;
+
+import com.docker.k8s.employeeservice.dto.EmployeeDto;
+import com.docker.k8s.employeeservice.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    EmployeeDto saveEmployee(@RequestBody EmployeeDto employeeDto) {
+        return employeeService.saveEmployee(employeeDto);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    EmployeeDto findEmployeeById(@PathVariable UUID id){
+        return employeeService.findEmployeeById(id);
+    }
+}
