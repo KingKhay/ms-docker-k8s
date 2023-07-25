49947169dbdb83b240308f561edde7b3737287f3
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-25 14:26:15 +0000

    refactor department service

--- department-service/src/main/java/com/docker/k8s/departmentservice/service/DepartmentService.java
+++ department-service/src/main/java/com/docker/k8s/departmentservice/service/DepartmentService.java
@@ -1,6 +1,8 @@
 package com.docker.k8s.departmentservice.service;
 
 import com.docker.k8s.departmentservice.model.Department;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
@@ -9,4 +11,8 @@ public interface DepartmentService {
     Department findDepartmentOfEmployee(UUID employeeId);
 
     Department saveDepartment(Department department);
+
+    Department findDepartmentById(UUID id);
+
+    Page<Department> findAllDepartments(Pageable pageable);
 }
--- department-service/src/main/java/com/docker/k8s/departmentservice/service/impl/DepartmentServiceImpl.java
+++ department-service/src/main/java/com/docker/k8s/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -5,6 +5,8 @@ import com.docker.k8s.departmentservice.model.Department;
 import com.docker.k8s.departmentservice.repository.DepartmentRepository;
 import com.docker.k8s.departmentservice.service.DepartmentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -13,7 +15,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class DepartmentServiceImpl implements DepartmentService {
 
-    private DepartmentRepository departmentRepository;
+    private final DepartmentRepository departmentRepository;
     @Override
     public Department findDepartmentOfEmployee(UUID employeeId) {
         return departmentRepository.findByEmployeeIdsContains(employeeId)
@@ -23,5 +25,16 @@ public class DepartmentServiceImpl implements DepartmentService {
     @Override
     public Department saveDepartment(Department department) {
         return departmentRepository.save(department);
+    }
+
+    @Override
+    public Department findDepartmentById(UUID id) {
+        return departmentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("No such department"));
+    }
+
+    @Override
+    public Page<Department> findAllDepartments(Pageable pageable) {
+        return departmentRepository.findAll(pageable);
     }
 }
