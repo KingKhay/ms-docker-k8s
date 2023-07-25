3197f9ab8b1883655e3ebdc9e52948e7ec55ba25
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-25 14:23:51 +0000

    handled operation failed exception in the global exception handler

--- department-service/src/main/java/com/docker/k8s/departmentservice/exception/GlobalEntityExceptionHandler.java
+++ department-service/src/main/java/com/docker/k8s/departmentservice/exception/GlobalEntityExceptionHandler.java
@@ -3,6 +3,7 @@ package com.docker.k8s.departmentservice.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
@@ -11,9 +12,17 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler({EntityNotFoundException.class})
     ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException exception){
         Map<String, Object> error = new HashMap<>();
         error.put("message", exception.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({OperationFailedException.class})
+    ResponseEntity<Object> handleOperationFailedException(OperationFailedException exception){
+        Map<String, Object> error = new HashMap<>();
+        error.put("message", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
