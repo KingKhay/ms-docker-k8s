46ebc04701184065929eed242238dbb7c257c5ec
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-25 07:34:11 +0000

    added repository for department

---
+++ department-service/src/main/java/com/docker/k8s/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package com.docker.k8s.departmentservice.repository;
+
+import com.docker.k8s.departmentservice.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface DepartmentRepository extends JpaRepository<Department, UUID> {
+
+    Optional<Department> findByEmployeeIdsContains(UUID employeeId);
+}
