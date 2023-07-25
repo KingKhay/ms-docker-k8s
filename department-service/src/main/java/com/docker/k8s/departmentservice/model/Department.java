b4ba3dc422692bf3395c2e4b9263860d79617ed6
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-25 14:24:19 +0000

    refactor department entity

--- department-service/src/main/java/com/docker/k8s/departmentservice/model/Department.java
+++ department-service/src/main/java/com/docker/k8s/departmentservice/model/Department.java
@@ -1,10 +1,7 @@
 package com.docker.k8s.departmentservice.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -14,6 +11,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Department {
 
     @Id
