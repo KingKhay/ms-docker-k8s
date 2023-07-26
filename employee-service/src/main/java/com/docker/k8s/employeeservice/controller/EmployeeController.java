7272202d93950b549a9cdd20da53980b1dfbfc71
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-26 11:08:56 +0000

    added endpoint to get all employees

--- employee-service/src/main/java/com/docker/k8s/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/docker/k8s/employeeservice/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.docker.k8s.employeeservice.controller;
 import com.docker.k8s.employeeservice.dto.EmployeeDto;
 import com.docker.k8s.employeeservice.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,5 +27,10 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.OK)
     EmployeeDto findEmployeeById(@PathVariable UUID id){
         return employeeService.findEmployeeById(id);
+    }
+
+    @GetMapping
+    Page<EmployeeDto> findEmployees(Pageable pageable){
+        return employeeService.findAllEmployees(pageable);
     }
 }
