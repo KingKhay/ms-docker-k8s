0fd23a436f7981e9aaa5b1cc91b5b64bf6f89a79
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-26 09:08:58 +0000

    refactor department model

--- department-service/src/main/java/com/docker/k8s/departmentservice/model/Department.java
+++ department-service/src/main/java/com/docker/k8s/departmentservice/model/Department.java
@@ -3,6 +3,7 @@ package com.docker.k8s.departmentservice.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,8 +18,17 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true)
     private String name;
 
     @ElementCollection
     private List<UUID> employeeIds;
+
+    public void addEmployeeId(UUID id){
+        if(employeeIds == null){
+            employeeIds = new ArrayList<>();
+        }
+        employeeIds.add(id);
+    }
 }
