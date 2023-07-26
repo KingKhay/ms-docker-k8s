e93d4a1ab0e853859efb435f0f19c35b92ba0fc5
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-26 09:11:29 +0000

    refactor employee-service

--- employee-service/src/main/java/com/docker/k8s/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/docker/k8s/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.docker.k8s.employeeservice.service.impl;
 
 import com.docker.k8s.employeeservice.client.DepartmentExchangeProxy;
+import com.docker.k8s.employeeservice.dto.BasicResponse;
 import com.docker.k8s.employeeservice.dto.EmployeeDto;
+import com.docker.k8s.employeeservice.dto.EmployeeRequest;
 import com.docker.k8s.employeeservice.exception.OperationFailedException;
 import com.docker.k8s.employeeservice.model.Department;
 import com.docker.k8s.employeeservice.model.Employee;
@@ -9,8 +11,11 @@ import com.docker.k8s.employeeservice.repository.EmployeeRepository;
 import com.docker.k8s.employeeservice.service.EmployeeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.UUID;
 
@@ -21,13 +26,20 @@ public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final ObjectMapper mapper;
     private final DepartmentExchangeProxy departmentExchangeProxy;
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     @Override
     @Transactional
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
         Employee savedEmployee = employeeRepository.save(mapper.convertValue(employeeDto, Employee.class));
         //Make call to department service to save department with user
-
+        try{
+            BasicResponse response = departmentExchangeProxy.saveEmployeeIdToDepartment(new EmployeeRequest(employeeDto.getDepartment().getName(), savedEmployee.getId()));
+            logger.info(response.getMessage());
+        }catch(WebClientResponseException exception){
+            logger.error("Error saving employee");
+            throw new OperationFailedException("Error while saving employee");
+        }
         return mapper.convertValue(savedEmployee, EmployeeDto.class);
     }
 
@@ -37,7 +49,13 @@ public class EmployeeServiceImpl implements EmployeeService {
                 .orElseThrow(() -> new OperationFailedException("Could not find employee"));
 
         //Make call to department service to get department of user.
-        Department department = departmentExchangeProxy.getDepartment(employee.getId());
+        Department department;
+        try{
+            department = departmentExchangeProxy.getDepartment(employee.getId());
+        }catch(WebClientResponseException exception){
+            logger.error("Error saving employee");
+            throw new OperationFailedException("Error while saving employee");
+        }
         EmployeeDto employeeDto = mapper.convertValue(employee, EmployeeDto.class);
         employeeDto.setDepartment(department);
         return employeeDto;
