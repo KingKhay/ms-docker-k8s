3c732d69486203364ce4900a6ce07425988951a9
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-08-07 10:44:50 +0000

    refactor employee request dto

--- department-service/src/main/java/com/docker/k8s/departmentservice/dto/EmployeeRequest.java
+++ department-service/src/main/java/com/docker/k8s/departmentservice/dto/EmployeeRequest.java
@@ -1,10 +1,12 @@
 package com.docker.k8s.departmentservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
 public class EmployeeRequest {
 
     private String department;
